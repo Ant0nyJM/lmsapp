6d67cb11d2cce7c8360c44fb8292515401f3905b
Nipun Shaji <nipunshaji9447@gmail.com>
2019-09-13 01:36:17 +0530

    sep-12-m

--- lms/settings.py
+++ lms/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'lms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'lmsdb.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lmsdb',
+        'USER':'lmsuser',
+        'PASSWORD':'lmsuser01',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
 
