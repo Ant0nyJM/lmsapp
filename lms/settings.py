a17c194128e322243f429934908d0afec6d370fa
Nipun Shaji <nipunshaji9447@gmail.com>
2019-09-13 08:28:43 +0530

    sep12-m9

--- lms/settings.py
+++ lms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n4cxuq!es5yxt7bznbbf8o^olhl$einyv+p3len1*3=8ri69md'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0']
 
