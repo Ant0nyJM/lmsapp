37aed0976c5450220c10ca2990a90b2c608447a4
Nipun Shaji <nipunshaji9447@gmail.com>
2019-09-13 08:23:31 +0530

    sep12-m7

--- lms/settings.py
+++ lms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n4cxuq!es5yxt7bznbbf8o^olhl$einyv+p3len1*3=8ri69md'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
