7edb2afa8e9117ee6b665e34bd0c087e273f61be
Ant0nyJM <antonyjm1997@gmail.com>
2019-09-17 16:00:09 +0530

    sep17-m2

--- lms/settings.py
+++ lms/settings.py
@@ -76,28 +76,28 @@ WSGI_APPLICATION = 'lms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'lmsdb',
-         'USER':'lmsuser',
-         'PASSWORD':'lmsuser01',
-         'HOST':'127.0.0.1',
-         'PORT':'5432',
-     }
-}
-
-
 # DATABASES = {
 #      'default': {
 #          'ENGINE': 'django.db.backends.postgresql',
-#          'NAME': 'def2tfi3vdvbqf',
-#          'USER':'lcxxsxcstmbqlx',
-#          'PASSWORD':'6f21e53ecfec25b3ed91eba6a3b40c345b1e0a5bb37ab4b128133fc4df12916f',
-#          'HOST':'ec2-174-129-229-106.compute-1.amazonaws.com',
+#          'NAME': 'lmsdb',
+#          'USER':'lmsuser',
+#          'PASSWORD':'lmsuser01',
+#          'HOST':'127.0.0.1',
 #          'PORT':'5432',
 #      }
-#  }
+# }
+
+
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'def2tfi3vdvbqf',
+         'USER':'lcxxsxcstmbqlx',
+         'PASSWORD':'6f21e53ecfec25b3ed91eba6a3b40c345b1e0a5bb37ab4b128133fc4df12916f',
+         'HOST':'ec2-174-129-229-106.compute-1.amazonaws.com',
+         'PORT':'5432',
+     }
+ }
 
 
 # Password validation
--- lmsapp/urls.py
+++ lmsapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
+    path('',views.book_search),
     path('signup/',views.signup,name='signup'),
     path('login/',views.userlogin,name='login'),
     path('logout/',views.userlogout,name='logout'),
